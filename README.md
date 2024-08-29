622489624f324f397cdc877748f4f1b826da977a
peerajak <peerajak@gmail.com>
2024-08-29 06:26:52 +0000

    task1 completed

--- README.md
+++ README.md
@@ -4,3 +4,23 @@
 
 This respository contains files required by the checkpoint Nr 15 project.
 Students must clone this respository into their ros2_ws workspace as instructed by the project.
+
+
+## Main Launch
+Terminal 1
+ros2 launch rb1_ros2_description rb1_ros2_xacro.launch.py
+
+## To test moving the robot
+Terminal x
+ros2 topic pub --rate 10 /rb1_robot/diffbot_base_controller/cmd_vel_unstamped geometry_msgs/msg/Twist "{linear: {x: 0.1, y: 0, z: 0.0}, angular: {x: 0.0,y: 0.0, z: 0.0}}"
+
+## To list all ros2 controllers
+ros2 control list_controllers --controller-manager /rb1_robot/controller_manager
+
+## To list all ros2 controllers' hardware interface
+ros2 control list_hardware_interfaces --controller-manager /rb1_robot/controller_manager
+
+### Helper commands
+
+#### Translate xacro to urdf
+xacro xacro/rb1_ros2_base.urdf.xacro  robot_name:=rb1_robot > translated_rb1_ros2_base.urdf
--- rb1_ros2_description/launch/rb1_ros2_xacro.launch.py
+++ rb1_ros2_description/launch/rb1_ros2_xacro.launch.py
@@ -7,6 +7,8 @@ from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 from ament_index_python.packages import get_package_prefix
 from launch_ros.descriptions import ParameterValue
+from launch.actions import ExecuteProcess, IncludeLaunchDescription, RegisterEventHandler, TimerAction
+from launch.event_handlers import OnProcessExit, OnProcessIO, OnProcessStart
 
 
 def generate_launch_description():
@@ -54,6 +56,19 @@ def generate_launch_description():
 
     robot_name_1 = "rb1_robot"
 
+    joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster",
+                   "--controller-manager", robot_name_1 + "/controller_manager"],
+    )
+    diffdrive_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["diffbot_base_controller",
+                   "--controller-manager", robot_name_1 +"/controller_manager"],
+    )
+
     rsp_robot1 = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
@@ -72,7 +87,34 @@ def generate_launch_description():
     )
 
     return LaunchDescription([
-        gazebo,
-        rsp_robot1,
-        spawn_robot1,
-    ])
+            gazebo,
+            rsp_robot1,
+            RegisterEventHandler(
+                event_handler=OnProcessStart(
+                    target_action=rsp_robot1,
+                    on_start=[
+                        TimerAction(
+                            period=10.0, # takes about 6-10 seconds for gazebo to load
+                            actions=[spawn_robot1],
+                        )
+                    ]
+                )
+            ),
+            RegisterEventHandler(
+                event_handler=OnProcessExit(
+                    target_action=spawn_robot1,
+                    on_exit=[
+                        TimerAction(
+                            period=120.0, # takes about 100-110 seconds for spawn to complete
+                            actions=[joint_state_broadcaster_spawner],
+                        )
+                    ],
+                )
+            ),
+            RegisterEventHandler(
+                event_handler=OnProcessExit(
+                    target_action=joint_state_broadcaster_spawner,
+                    on_exit=[diffdrive_controller_spawner],
+                )
+            )
+        ])
